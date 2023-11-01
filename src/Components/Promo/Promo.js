7f054817d23e50ecc193058696b2cfb921775339
vitassablino <vitassablino1@gmail.com>
2023-11-01 14:21:33 +0300

    finished promo

--- src/Components/Promo/Promo.js
+++ src/Components/Promo/Promo.js
@@ -1,3 +1,4 @@
+import "./Promo.css"
 import landing_logo from "../../images/pic__COLOR_landing-logo.svg";
 
 function Promo() {
@@ -7,14 +8,7 @@ function Promo() {
         <h1 className="promo__title">
           Учебный проект студента факультета Веб&#8209;разработки.
         </h1>
-        <button
-          className="promo__btn-anchor hover-button"
-          type="button"
-        >
-          Узнать больше
-        </button>
       </div>
-      <img className="promo__img" src={landing_logo} alt="Изображение" />
     </section>
   );
 }
