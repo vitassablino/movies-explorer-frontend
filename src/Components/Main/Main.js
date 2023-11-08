1e0bee3b0170dc422fe9b813d241e124876a1cd8
vitassablino <vitassablino1@gmail.com>
2023-11-08 20:01:59 +0300

    started Tecnologies. Need to Unite section title

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,6 +3,7 @@ import "./Main.css";
 import Promo from "../Promo/Promo";
 import NavBar from "../NavBar/NavBar";
 import AboutProject from "../AboutProject/AboutProject";
+import Technologies from "../Technologies/Technologies";
 
 function Main({ aboutRef }) {
   return (
@@ -10,6 +11,7 @@ function Main({ aboutRef }) {
       <Promo  />
       <NavBar />
       <AboutProject aboutRef={aboutRef} />
+      <Technologies />
     </main>
   );
 }
---
+++ src/Components/Technologies/Technologies.js
@@ -0,0 +1,40 @@
+import "./Technologies.css";
+import GreeteingTitle from "../GreetingTitle/GreeteingTitle";
+
+function Technologies() {
+  return (
+    <section className="technologies">
+      <GreeteingTitle title="Технологии" />
+      <h3 className="technologies__title">7 технологий</h3>
+      <p className="technologies__text">
+        На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые
+        применили в&nbsp;дипломном проекте.
+      </p>
+      <ul className="technologies__list">
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">HTML</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">CSS</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">JS</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">React</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">Git</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">Express.js</p>
+        </li>
+        <li className="technologies__list-item">
+          <p className="technologies__list-item-text">mongoDB</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Technologies;
